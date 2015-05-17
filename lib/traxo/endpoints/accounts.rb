47d6c5a41fd4abcc9fdf570a508dba5515334bef
Wil Chandler <wilchandler2@gmail.com>
2015-05-17 15:27:57 -0500

    Implemented Traxo::Client#get_accounts

--- lib/traxo/endpoints/accounts.rb
+++ lib/traxo/endpoints/accounts.rb
@@ -6,6 +6,21 @@ module Traxo
       response = get_request_with_token(url)
       Account.new(response)
     end
+
+    def get_accounts(options = {})
+      data = get_accounts_options(options)
+      url = "#{ API_URL}accounts#{ query_string(data) }"
+      response = get_request_with_token(url)
+      response.map { |account| Account.new(account) }
+    end
+
+      private
+
+    def get_accounts_options(args)
+      options = [:status, :classification, :offset, :limit]
+      args.select! { |a| options.include? a }
+      args
+    end
   end
 
 end
--- spec/endpoints/accounts_spec.rb
+++ spec/endpoints/accounts_spec.rb
@@ -25,4 +25,35 @@ describe 'Traxo::Client accounts endpoints' do
       expect(result).to be_instance_of Traxo::Account
     end
   end
+
+  describe '#get_accounts' do
+    let(:address) { "#{Traxo::Client::API_URL}accounts" }
+    let(:call) { client.get_accounts }
+    let(:fixture_response) { File.new("#{FIXTURES_DIR}/client/accounts/accounts.json") }
+    let(:stub) { stub_request(:get, address).with(headers: headers).to_return(body: fixture_response) }
+
+    it 'makes a GET request to the correct address with an address token in the headers' do
+      stub && call
+
+      expect(stub).to have_been_requested
+    end
+
+    it 'returns an array of Traxo::Account objects' do
+      stub
+      result = call
+      types = result.map(&:class).uniq
+
+      expect(result).to be_instance_of Array
+      expect(types).to match_array [Traxo::Account]
+    end
+
+    it 'accepts options: status, classification, offset, limit' do
+      args = { status: 'Active', classification: 'Hotel', offset: 2, limit: 5 }
+      options_stub = stub_request(:get, address).with(headers:headers, query: args)
+                                                .to_return(body: fixture_response)
+      client.get_accounts(args)
+
+      expect(options_stub).to have_been_requested
+    end
+  end
 end
