c0d6ffdfdebb7ec1deee13d7f5e37b23418c6eee
Wil Chandler <wilchandler2@gmail.com>
2015-06-28 16:13:05 -0500

    Started implementing car segments

--- lib/traxo/endpoints/segments/car.rb
+++ lib/traxo/endpoints/segments/car.rb
@@ -1,18 +1,44 @@
 module Traxo
   class Client
-    def get_car_segments(options = {})
+    def get_car_segments(args = {})
+      query = get_car_segments_options(args)
+      url = "#{ API_URL }segments/car#{ query_string(query) }"
+      get_request_with_token(url)
     end
 
     def get_car_segment(id)
+      url = "#{ API_URL}segments/car/#{id}"
+      get_request_with_token(url)
     end
 
     def create_car_segment(args)
+      url = "#{ API_URL}segments/car"
+      data = create_car_segment_options(args)
+      post_request_with_token(url, data)
     end
 
-    def update_air_segment(id, args)
+    def update_car_segment(id, args)
+      url = "#{ API_URL}segments/car/#{id}"
+      data = create_car_segment_options(args, false)
+      data.delete_if { |key, val| !val } # previous datetime filtering is skipped
+      raise ArgumentError.new('Must provide options to update segment') if data.empty?
+      put_request_with_token(url, data)
     end
 
-    def delete_air_segment(id)
+    def delete_car_segment(id)
+        url = "#{ API_URL }segments/car/#{id}"
+        delete_request_with_token(url)
     end
+
+      private
+
+    def get_car_segments_options(args)
+      # 
+    end
+
+    def create_car_segment_options(args)
+      #
+    end
+
   end
 end
